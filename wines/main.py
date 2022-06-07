d64184f585aaa017bdfbf63948dbbabb7799ae11
RafaDVB <rafael.v.barrientos@gmail.com>
2022-06-06 23:50:58 -0400

    Preliminar, elegir otros modelos y revisar resultados

--- wines/main.py
+++ wines/main.py
@@ -2,6 +2,15 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 
 DATA_PATH = "wines/wines.csv"
 
@@ -22,18 +31,73 @@ def load_data(data_path = DATA_PATH):
     return pd.read_csv(data_path)
 
 # 1.Generacion del conjunto de prueba
+np.random.seed(42)
 wine_data = load_data()
-wine_data.hist(bins=20, figsize=(10, 10))
-save_fig("histogram")
-plt.show()
+X = wine_data.drop("quality", axis = 1)
+y = wine_data.get("quality").to_numpy()
+
+correlation = wine_data.corr()
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42, stratify=y)
 
 # 2.Exploracion de datos
 
+# Distribucion
+wine_data.hist(bins=50, figsize=(8, 8))
+save_fig("wine-histogram")
+plt.show()
+
+# Escala y rango de valores
+
+# Correlacion
+plt.matshow(correlation, cmap = "inferno")
+plt.xticks(range(wine_data.select_dtypes(["number"]).shape[1]), wine_data.select_dtypes(["number"]).columns, rotation = 90)
+plt.yticks(range(wine_data.select_dtypes(["number"]).shape[1]), wine_data.select_dtypes(["number"]).columns)
+plt.colorbar()
+save_fig("wine-correlation")
+plt.show()
 
 # 3.Preparacion de datos
 
+# Escalar
+num_pipeline = Pipeline([
+        ('imputer', SimpleImputer(strategy="median")),
+        ('std_scaler', StandardScaler())
+    ])
+wine_data_tr = num_pipeline.fit_transform(wine_data)
+print(wine_data_tr)
+
 # 4.Exploracion de modelos (3 modelos distintos)
+score_list = []
+
+def cross_validation(model, title):
+    scores = cross_val_score(model, wine_data_tr, y, cv=5)
+    acc = scores.mean()
+    print("%s accuracy: %0.4f (+/- %0.4f)" % (title, acc, scores.std() * 2))
+    score_list.append({"score": acc, "name": title})
+    return acc
+
+# K-Nearest
+knclf = KNeighborsClassifier()
+knclf.fit(X_train, y_train)
+cross_validation(knclf, "K-Nearest Neighbors")
+
+# Decision Tree
+dtclf = DecisionTreeClassifier()
+dtclf.fit(X_train, y_train)
+cross_validation(dtclf, "Decision Tree")
+
+# Random Forest
+rfclf = RandomForestClassifier()
+rfclf.fit(X_train, y_train)
+cross_validation(rfclf, "Random Forest")
+
+# SVM
+svmclf = SVC()
+svmclf.fit(X_train, y_train)
+cross_validation(svmclf, "SVC")
 
 # 5.Afinacion de modelos
+
 
 # 6.Solucion
