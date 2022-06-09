de36488daa6c19dc27f73b595f6389bb941ebeb2
Luis Alejandro Castillo Martínez <luiscasm2501@gmail.com>
2022-06-08 21:43:44 -0400

    Model tuning final

--- who/main.py
+++ who/main.py
@@ -1,20 +1,18 @@
 import pandas as pd
-from pandas.plotting import scatter_matrix
 import sys
 import os
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from sklearn import naive_bayes
 from sklearn.compose import make_column_transformer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, plot_confusion_matrix, precision_score, recall_score
-from sklearn.model_selection import GridSearchCV, RandomizedSearchCV, cross_val_score, cross_validate, train_test_split
+from sklearn.metrics import ConfusionMatrixDisplay, accuracy_score, confusion_matrix, precision_score, recall_score
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV, cross_val_score, train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder, OrdinalEncoder, StandardScaler
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, StandardScaler
 from dotenv import load_dotenv
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
@@ -91,10 +89,10 @@ print("CORRELATIONS\n", corr_matrix["remainder__stroke"].sort_values(
 processed_who_data["age_cat"] = pd.cut(
     processed_who_data["pipeline__age"], 4, labels=[1, 2, 3, 4])
 
-processed_who_data.hist(bins=50, figsize=(40, 30))
+processed_who_data.hist(bins=100, figsize=(40, 30))
 save_fig("processed_histogram_plots")
 
-processed_who_data["age_cat"].hist()
+processed_who_data["age_cat"].hist(bins=10, figsize=(20, 15))
 save_fig("stratified age")
 
 X = processed_who_data.drop(["remainder__stroke"], axis=1)
@@ -106,28 +104,20 @@ for Xi in (X_train, X_test, X):
     Xi.drop("age_cat", axis=1, inplace=True)
 
 
-""" Gotta look into binary classification algorithms:
-https://machinelearningmastery.com/types-of-classification-in-machine-learning/#:~:text=The%20class%20for%20the%20normal,probability%20distribution%20for%20each%20example.
-
-Popular algorithms are:
-- Logistic Regression
-- k-Nearest Neighbors
-- Decision Trees
-- Support Vector Machine
-- Naive Bayes
-"""
-
 score_list = list()
+
 
 def add_score(acc, title):
     score_list.append({"score": acc, "name": title})
-        
+
+
 def cross_validation(model, title):
     scores = cross_val_score(model, X, y, cv=5)
     acc = scores.mean()
     print("%s accuracy: %0.4f (+/- %0.4f)" % (title, acc, scores.std() * 2))
     add_score(acc, title)
     return acc
+
 
     # K-Nearest
 knc_clf = KNeighborsClassifier()
@@ -158,40 +148,95 @@ for i, result in enumerate(score_list):
 # LET's TUNE Log reg, SVC, and Rand For
 # First we do random to have an idea where to test, then grid search
 
-# Log reg:
-lr_params = {
-    "C": list(np.arange(0.1, 1, 0.01)),
-    "class_weight": [None],
-    "solver": ["lbfgs"]
-}
 n_iter = 50
-score_list = list()
+final_score_list = list()
+
+
 def tune_model(model, params, title):
-    grid_search = GridSearchCV(estimator=model, param_grid = params, cv=5)
+    grid_search = GridSearchCV(estimator=model, param_grid=params, cv=5)
     grid_search.fit(X_train, y_train)
     acc = grid_search.best_score_
-    print(f"{title}:\n\tBEST PARAMS: {grid_search.best_params_}\n\tTRAIN SCORE:{acc}\n")    
+    print(
+        f"\n{title}:\n\tBEST PARAMS: {grid_search.best_params_}\n\tTRAIN SCORE:{acc}\n\t")
+
     best_model = grid_search.best_estimator_
     y_pred = best_model.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
-    # TODO: recall is not working properly (shows zero)
     rec = recall_score(y_test, y_pred, labels=np.unique(y_pred))
-    pre = precision_score(y_test, y_pred, average='weighted', labels=np.unique(y_pred))
-    plot_confusion_matrix(best_model, X_test, y_test, cmap='GnBu')
-    print(acc,rec,pre)
-    save_fig(f"confusion_matrix_{title}")
-    score_list.append({"accuracy": acc, "recall":rec, "precision":pre, "average":np.mean([acc, rec, pre]),  "name": title})
-    print("\tTEST:\n\t ACCURACY: %0.4f \t RECALL: %0.4f \t PRECISION: %0.4f" % (acc,rec,pre))
-    score_list.append({"score": acc, "name": title})
+    pre = precision_score(
+        y_test, y_pred, average='weighted', labels=np.unique(y_pred))
 
+    conf_mat = confusion_matrix(y_test, y_pred)
+    display = ConfusionMatrixDisplay(
+        conf_mat, display_labels=best_model.classes_)
+    display.plot()
+    save_fig(f"confusion_matrix_{title}")
+
+    final_score_list.append({"accuracy": acc, "recall": rec, "precision": pre,
+                             "average": np.mean([acc, rec, pre]),  "name": title})
+    print("\n\tTEST DATA RESULTS:\n\tACCURACY: %0.6f \t RECALL: %0.4f \t PRECISION: %0.6f" %
+          (acc, rec, pre))
+
+
+# Logistic Regression
+lr_params = {
+    "C": list(np.arange(0.1, 1, 0.01)),
+    "class_weight": [None],
+    "solver": ["lbfgs"],
+}
 
 lr_rscv = RandomizedSearchCV(
     estimator=lr_clf, param_distributions=lr_params, n_iter=n_iter, random_state=RANDOM_STATE)
 
 
 lr_rscv.fit(X_train, y_train)
-# TEST 
 
-lr_params["C"] = [0.1, 0.4, 0.6, 1.]
+lr_params["C"] = [0.1, 0.2, 0.3, 0.4, 0.5]
 
-tune_model(lr_clf, lr_params, "Linear Regression" )
+tune_model(lr_clf, lr_params, "Logistic Regression")
+
+# SVC
+svc_params = {
+    "C": list(np.arange(1, 100, 1)),
+    "kernel": ["poly", "rbf"],
+    "degree": [3, 5, 7, 9, 10],
+    "gamma": ["scale", "auto"],
+}
+
+
+svc_rscv = RandomizedSearchCV(
+    estimator=svc_clf, param_distributions=svc_params, n_iter=n_iter, random_state=RANDOM_STATE)
+
+
+svc_rscv.fit(X_train, y_train)
+
+print("SVC RANDOM SEARCH: ", svc_rscv.best_params_)
+
+svc_params = {
+    "C": [0.5, 1, 1.5],
+    "kernel": ["rbf"],
+    "gamma": ["scale"]
+}
+
+tune_model(svc_clf, svc_params, "SVC")
+
+# Random Forest
+rfc_params = {
+    "n_estimators": list(range(1, 1000, 10)),
+    "max_features": ["sqrt", "log2"]
+}
+
+""" #(commented to execute script faster)
+rfc_rscv = RandomizedSearchCV(
+estimator=rfc_clf, param_distributions=rfc_params, n_iter=n_iter, random_state=RANDOM_STATE, n_jobs=2)
+
+rfc_rscv.fit(X_train, y_train)
+
+print("RANDOM FOREST RANDOM SEARCH: ", rfc_rscv.best_params_)
+"""
+rfc_params = {
+    "n_estimators": [50, 75, 100, 125],
+    "max_features": ["sqrt", "log2"]
+}
+
+tune_model(rfc_clf, rfc_params, "Random Forest")
